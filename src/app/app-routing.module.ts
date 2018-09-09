16ed498d111710145a0933714fd3e6ef752e8481
Isaac Omondi <iomondi90@gmail.com>
2018-09-09 12:59:16 +0300

    initial commit: secure application and generate source files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'todo',
+    component: TodoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
